d05224dc92b16db21581d8edeaf05e461f743c40
Nitin <nitin.khandelwal@tothenew.com>
2021-02-15 21:46:03 +0530

    Q2 adjjusted

--- Q2.java
+++ Q2.java
@@ -1,14 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-
 public class Q2 {
     public static void main(String[] args) {
             String str = "Horse have four legs and one nose where as human have two legs and one nose";
             str = str.toLowerCase();
             String words[] = str.split(" ");
-            List<String> repeadedWords = new ArrayList<String>(words.length);
 
             for(int i=0; i<words.length; i++){
                 int count = 1;
@@ -20,11 +14,7 @@ public class Q2 {
                 }
                 if(count > 1 && words[i] != "0"){
                     System.out.println(words[i] + ": " + count);
-                    repeadedWords.add(words[i]);
                 }
-            }
-            for(int i=0;i<repeadedWords.size();i++){
-                System.out.println(repeadedWords.get(i));
             }
     }
 }
